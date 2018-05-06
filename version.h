48557ba7c94ebe12b8a047473af399986cbbae03
Soar Qin <soarchin@gmail.com>
2018-05-06 22:43:36 +0800

    WIP & Changes

--- version.h
+++ version.h
@@ -2,8 +2,8 @@
 #define __VERSION_H_
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 6
-#define VERSION_PATCH 1
-#define VERSION_STR "0.6.1"
+#define VERSION_MINOR 7
+#define VERSION_PATCH 0
+#define VERSION_STR "0.7"
 
 #endif
